041d55755e1ac09d4c883ce55e63cfd58aa797b7
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2024-12-10 18:48:55 -0500

    Add Grid Class and functionality to draw Tetris Grid on Window

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -1,1 +1,43 @@
 #include "Grid.h"
+#include <iostream>
+
+Grid::Grid() : m_numRows(20), m_numCols(10), m_cellSize(30)
+{
+	InitGrid();
+}
+
+void Grid::Print()
+{
+	for (int y = 0; y < m_numCols; y++)
+	{
+		for (int x = 0; x < m_numRows; x++)
+		{
+			std::cout << m_grid[x][y] << " ";
+		}
+		std::cout << std::endl; 
+	}
+}
+
+void Grid::Draw()
+{
+	for (int y = 0; y < m_numCols; y++)
+	{
+		for (int x = 0; x < m_numRows; x++)
+		{
+			int cellValue = m_grid[x][y];
+			DrawRectangle(y * m_cellSize + 1, x * m_cellSize + 1, m_cellSize - 1, m_cellSize - 1, Colors::cellColors[cellValue]);
+		}
+	}
+
+}
+
+void Grid::InitGrid()
+{
+	for (int y = 0; y < m_numCols; y++)
+	{
+		for (int x = 0; x < m_numRows; x++)
+		{
+			m_grid[x][y] = 0;
+		}
+	}
+}
--- src/Grid.h
+++ src/Grid.h
@@ -1,10 +1,26 @@
 #include "raylib.h"
+#include "Colors.h"
 
 #ifndef GRID_H
 #define GRID_H
 
 class Grid
 {
+public:
+	Grid();
+	void Print();
+	void Draw();
+
+	// Member Variables
+	int m_grid[20][10];
+
+private:
+	void InitGrid();
+
+	// Member Variables
+	int m_numRows;
+	int m_numCols;
+	int m_cellSize;
 };
 
 #endif // !GRID_H
