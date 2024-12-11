84467447daaf997d02fd9ba254c458a0aa93e9f1
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2024-12-10 22:30:07 -0500

    Add Tetrominos classes

---
+++ src/Tetrominos.cpp
@@ -0,0 +1,64 @@
+#include "Tetrominos.h"
+
+Tetromino::LTetromino::LTetromino()
+{
+	m_id = 2;
+	m_cells[0] = { Position {0,2}, Position {1,0}, Position {1,1}, Position {1,2} };
+	m_cells[1] = { Position {0,1}, Position {1,1}, Position {2,1}, Position {2,2} };
+	m_cells[2] = { Position {2,0}, Position {1,0}, Position {1,1}, Position {1,2} };
+	m_cells[3] = { Position {0,0}, Position {0,1}, Position {1,1}, Position {2,1} };
+}
+
+Tetromino::ITetromino::ITetromino()
+{
+	m_id = 7;
+	m_cells[0] = { Position {1,0}, Position {1,1}, Position {1,2}, Position {1,3} };
+	m_cells[1] = { Position {0,2}, Position {1,2}, Position {2,2}, Position {3,2} };
+	m_cells[2] = { Position {2,0}, Position {2,1}, Position {2,2}, Position {2,3} };
+	m_cells[3] = { Position {0,1}, Position {1,1}, Position {2,1}, Position {3,1} };
+}
+
+Tetromino::OTetromino::OTetromino()
+{
+	m_id = 4;
+	m_cells[0] = { Position {0,0}, Position {0,1}, Position {1,0}, Position {1,1} };
+	m_cells[1] = { Position {0,0}, Position {0,1}, Position {1,0}, Position {1,1} };
+	m_cells[2] = { Position {0,0}, Position {0,1}, Position {1,0}, Position {1,1} };
+	m_cells[3] = { Position {0,0}, Position {0,1}, Position {1,0}, Position {1,1} };
+}
+
+Tetromino::JTetromino::JTetromino()
+{
+	m_id = 1;
+	m_cells[0] = { Position {0,0}, Position {1,0}, Position {1,1}, Position {1,2} };
+	m_cells[1] = { Position {0,1}, Position {0,2}, Position {1,1}, Position {2,1} };
+	m_cells[2] = { Position {1,0}, Position {1,1}, Position {1,2}, Position {2,2} };
+	m_cells[3] = { Position {0,1}, Position {1,1}, Position {2,0}, Position {2,1} };
+}
+
+Tetromino::STetromino::STetromino()
+{
+	m_id = 6;
+	m_cells[0] = { Position {0,1}, Position {0,2}, Position {1,0}, Position {1,1} };
+	m_cells[1] = { Position {0,1}, Position {0,2}, Position {1,1}, Position {2,1} };
+	m_cells[2] = { Position {1,0}, Position {1,1}, Position {1,2}, Position {2,2} };
+	m_cells[3] = { Position {0,1}, Position {1,1}, Position {2,0}, Position {2,1} };
+}
+
+Tetromino::ZTetromino::ZTetromino()
+{
+	m_id = 1;
+	m_cells[0] = { Position {0,0}, Position {1,0}, Position {1,1}, Position {1,2} };
+	m_cells[1] = { Position {0,1}, Position {0,2}, Position {1,1}, Position {2,1} };
+	m_cells[2] = { Position {1,0}, Position {1,1}, Position {1,2}, Position {2,2} };
+	m_cells[3] = { Position {0,1}, Position {1,1}, Position {2,0}, Position {2,1} };
+}
+
+Tetromino::TTetromino::TTetromino()
+{
+	m_id = 1;
+	m_cells[0] = { Position {0,0}, Position {1,0}, Position {1,1}, Position {1,2} };
+	m_cells[1] = { Position {0,1}, Position {0,2}, Position {1,1}, Position {2,1} };
+	m_cells[2] = { Position {1,0}, Position {1,1}, Position {1,2}, Position {2,2} };
+	m_cells[3] = { Position {0,1}, Position {1,1}, Position {2,0}, Position {2,1} };
+}
---
+++ src/Tetrominos.h
@@ -0,0 +1,46 @@
+#include "Block.h"
+
+namespace Tetromino
+{
+	class LTetromino : public Block
+	{
+	public:
+		LTetromino();
+	};
+
+	class ITetromino : public Block
+	{
+	public:
+		ITetromino();
+	};
+
+	class OTetromino : public Block
+	{
+	public:
+		OTetromino();
+	};
+	
+	class JTetromino : public Block
+	{
+	public:
+		JTetromino();
+	};
+
+	class STetromino : public Block
+	{
+	public:
+		STetromino();
+	};
+
+	class ZTetromino : public Block
+	{
+	public:
+		ZTetromino();
+	};
+
+	class TTetromino : public Block
+	{
+	public:
+		TTetromino();
+	};
+}
