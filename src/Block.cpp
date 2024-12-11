22b59df0ddb1bce233b39687e3c601fb39daecb2
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2024-12-10 22:29:32 -0500

    Updated Block to be a base class for future Tetrominos

--- src/Block.cpp
+++ src/Block.cpp
@@ -1,1 +1,15 @@
 #include "Block.h"
+
+Block::Block() : m_cellSize(30), m_rotationState(0), m_id(0)
+{
+}
+
+void Block::Draw()
+{
+	std::array<Position, 4>& tiles = m_cells[m_rotationState];
+	for (int i = 0; i < tiles.size(); i++)
+	{
+		DrawRectangle(tiles[i].coordinate[1] * m_cellSize + 1, tiles[i].coordinate[0] * m_cellSize + 1, 
+			m_cellSize - 1, m_cellSize - 1, Colors::cellColors[m_id]);
+	}
+}
--- src/Block.h
+++ src/Block.h
@@ -1,12 +1,21 @@
 #include "raylib.h"
+#include "Colors.h"
+#include "Position.h"
+#include <map>
 
 #ifndef BLOCK_H
 #define BLOCK_H
-struct Block
+class Block
 {
-	int x, y;
-	int size;
-	Color color;
+public:
+	Block();
+	void Draw();
+	int m_id;
+	std::map<int, std::array<Position, 4>> m_cells;
+
+private:
+	int m_cellSize;
+	int m_rotationState;
 };
 
 #endif // !BLOCK_H
