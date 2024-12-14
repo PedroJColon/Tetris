c579741c89746b2681ae4e3b8a3e74ca08e61895
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2024-12-13 23:49:49 -0500

    Add Input, BlockOutsideCheck, and Refill pieces function

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include <time.h>
 
 Game::Game() : m_SCREENWIDTH(300), m_SCREENHEIGHT(600), m_TITLE("Tetris - Raylib") { }
 
@@ -11,7 +12,9 @@ void Game::Run()
     //--------------------------------------------------------------------------------------
 
     m_grid.Print();
-
+    srand(time(NULL));
+    RefillPieces();
+    currentBlock = GetRandomPiece();
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -32,7 +35,8 @@ void Game::Update()
     //----------------------------------------------------------------------------------
     // TODO: Update your variables here
     //----------------------------------------------------------------------------------
-
+    float deltaTime = GetFrameTime();
+    Input(deltaTime);
 }
 
 void Game::Draw()
@@ -44,8 +48,93 @@ void Game::Draw()
     ClearBackground(Colors::blue);
 
     m_grid.Draw();
-    jTetromino.Draw();
+    currentBlock.Draw();
 
     EndDrawing();
     //----------------------------------------------------------------------------------
 }
+
+void Game::DrawPieces()
+{
+    for (size_t i = 0; i < m_pieces.size(); i++)
+    {
+        m_pieces[i].Draw();
+    }
+}
+
+void Game::Input(float deltaTime)
+{
+    if (IsKeyPressed(KEY_LEFT))
+    {
+        currentBlock.Move(0, -100 * deltaTime);
+        if (IsBlockOutside())
+        {
+            currentBlock.Move(0, 100 * deltaTime);
+        }
+        
+    }
+    if (IsKeyPressed(KEY_RIGHT))
+    {
+        currentBlock.Move(0, 100 * deltaTime);
+        if (IsBlockOutside())
+        {
+            currentBlock.Move(0, -100 * deltaTime);
+        }
+    }
+    if (IsKeyPressed(KEY_UP))
+    {
+        // Rotate Block
+    }
+    if (IsKeyPressed(KEY_DOWN))
+    {
+        currentBlock.Move(100 * deltaTime, 0);
+        if (IsBlockOutside())
+        {
+            currentBlock.Move(-100 * deltaTime, 0);
+        }
+
+    }
+}
+
+Block Game::GetRandomPiece()
+{
+    int sizeModifer = m_pieces.size();
+    for (size_t i = 0; i < m_pieces.size(); i++)
+    {
+        if (m_pieces[i].m_activeBlock)
+        {
+            --sizeModifer;
+        }
+        if (sizeModifer <= 0)
+        {
+            sizeModifer = m_pieces.size();
+        }
+    }
+    int randomIndex = rand() % m_pieces.size();
+    Block randBlock = m_pieces[randomIndex];
+    m_pieces[randomIndex] = m_pieces[randomIndex];
+    m_pieces[randomIndex].m_activeBlock = true;
+    
+    return randBlock;
+}
+
+bool Game::IsBlockOutside()
+{
+    for (size_t i = 0; i < currentBlock.GetCellPositions().size(); i++)
+    {
+        if (m_grid.IsCellOutside(currentBlock.GetCellPositions().at(i).coordinate[0], currentBlock.GetCellPositions().at(i).coordinate[1]))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+const void Game::RefillPieces()
+{
+    m_pieces = {
+     Tetromino::LTetromino(), Tetromino::ITetromino(), Tetromino::JTetromino(), Tetromino::OTetromino(),
+         Tetromino::STetromino(), Tetromino::TTetromino(), Tetromino::ZTetromino()
+    };
+}
+
--- src/Game.h
+++ src/Game.h
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include "Grid.h"
 #include "Tetrominos.h"
+#include <vector>
 
 #ifndef GAME_H
 #define GAME_H
@@ -16,15 +17,21 @@ private:
     const int m_SCREENHEIGHT;
     const char* m_TITLE;
 
+    std::array<Block, 7> m_pieces;
+    // Possible thought
+    // Create two arrays for pieces. One that is active and another that is not.
+    // Another way we can do this is by using bitmask flags to save on memory
+    Block currentBlock;
     Grid m_grid;
-    Tetromino::LTetromino lTetromino;
-    Tetromino::ITetromino iTetromino;
-    Tetromino::OTetromino oTetromino;
-    Tetromino::JTetromino jTetromino;
 
     // PRIVATE FUNCTIONS HERE
     void Update();
     void Draw();
+    void DrawPieces();
+    void Input(float deltaTime);
+    Block GetRandomPiece();
+    bool IsBlockOutside();
+    const void RefillPieces();
 };
 
 #endif // GAME_H
