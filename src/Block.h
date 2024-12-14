902e08ef9621a7060c55897391a5cdaab7ba89aa
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2024-12-13 23:49:15 -0500

    Update spacing and thinking about possible ways to make block active and inactive

--- src/Block.h
+++ src/Block.h
@@ -14,6 +14,11 @@ public:
 	std::array<Position, 4> GetCellPositions();
 	std::map<int, std::array<Position, 4>> m_cells;
 	int m_id;
+	// TODO/NOTE
+	// Add variable to indicate block is currently not in use
+	// Maybe can do bitwise operations to indicate if block is in use or not
+	// Or create a seperate vector or array list to check if active or not
+	bool m_activeBlock;
 
 private:
 	int m_rowOffset;
